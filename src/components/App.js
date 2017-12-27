bf34481d5a392f6061d4b86fbfec36e4b3c9d45e
Stanislav Zhukovskiy <stanislav@Mac-mini-Alexey.local>
2017-12-27 18:13:35 +0200

    add project

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Navigation from './Navigation';
+import './App.css';
+
+const App = () =>
+  <Router>
+    <Navigation />
+  </Router>
+
+export default App;
